c14bd30e4dbf19f88744ef6a1c4804e208fc06db
joy <joy.y@kakaocorp.com>
2021-10-02 15:32:19 +0900

    #4 joy.y

---
+++ 04_jump_the_five/jump.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Author : NowHappy <rlfmalehd@gmail.com>
+Date   : 2021-10-02
+Purpose: Jump the Five
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Jump the Five',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('input',
+                        metavar='str',
+                        help='Input text')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    text = args.input
+
+    jumper = { '1': '9',
+               '2': '8',
+               '3': '7',
+               '4': '6',
+               '5': '0',
+               '6': '4',
+               '7': '3',
+               '8': '2',
+               '9': '1',
+               '0': '5'
+               }
+
+    for char in args.input:
+        print(jumper.get(char, char), end='')
+    print()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
